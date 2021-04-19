f3dcc55dbf8cc19acec39ddc2254a5da7e87444d
raheela <raheelamoha@gmail.com>
2021-04-19 12:08:25 +0300

    adds padding to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () =>{
     return(
         <>
-        <div  className="fixed-top text-pink bg-white border-bottom w-100 px-3 d-flex justify-content-end ">
+        <div  className="fixed-top text-pink bg-white border-bottom w-100 pt-1 px-3 d-flex justify-content-end ">
             <h4 className="mr-auto font-weight-bolder">ribbon</h4>
             <h4 ><i className=" bi bi-person-circle bg-red mr-1"></i></h4>
             <h4 ><i className="bi bi-bell-fill"></i></h4>
